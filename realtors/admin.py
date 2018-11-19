73f74a5008c424a01d9284ef7d757dff4e283351
Phalguna kumar <pkrachoor@gmail.com>
2018-11-18 21:00:48 -0500

    added search functionality, dynamic listings render pages

--- realtors/admin.py
+++ realtors/admin.py
@@ -6,7 +6,7 @@ class RealtorAdmin(admin.ModelAdmin):
     list_display = ('id','name','email','hire_date')
     list_display_links = ('id','name')
     search_fields = ('name',)
-    list_filter = ('name', )
+    list_filter = ('name',)
     list_per_page = 25
 
 
